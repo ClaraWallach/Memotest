d60fc238ffaab55f01c23b0d0085d5f648c0c2bd
ClaraWallach <clari.wallach@gmail.com>
2021-07-20 14:27:06 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function() { 
+
+ /* Declaracion de variables */   
+    const cartas = document.querySelectorAll ('.carta');
+    let yaClickeo = false;
+    let bloquear = false; 
+    let seleccion1 = '';
+    let seleccion2 = '';
+
+/* Declaracion de funciones */
+
+(function ordenarCartas () {
+    cartas.forEach (function (carta){
+        numero = Math.floor(Math.random()*24 );
+        carta.style.order = numero; 
+    })
+} ) ();
+/* Los parentesis antes y despues de la declaracion de la funcion son para que se ejecute inmediatamente */
+/* Math.random() da un numero random entre 0 y 1, lo multiplico por 24 y Math.floor() lo convierte en entero */
+/* order es la propiedad de css que funciona con grid */
+
+function pareja() {
+
+        seleccion1.removeEventListener ('click', flip);
+        seleccion2.removeEventListener ('click', flip);
+        seleccion1 = '';
+        seleccion2 = '';
+    }
+
+    function noPareja() {
+
+        seleccion1.classList.remove ('flip');
+        seleccion2.classList.remove ('flip');
+        seleccion1 = '';
+        seleccion2 = '';
+        bloquear = false;
+        let numero = ''  
+    }
+    
+    
+
+    function flip () {
+
+        if (bloquear === false) {
+        
+            this.classList.add ('flip');
+
+            if (!yaClickeo) { 
+                /* Primer click */
+                seleccion1 = this;
+                yaClickeo =  true;
+            } else if (this !== seleccion1) /* Que no se pueda comparar consigo misma */ {
+                /* Segundo click */
+                seleccion2 = this; 
+                yaClickeo = false ;
+            }
+
+            if (seleccion1.classList[1] === seleccion2.classList[1] ) {
+                pareja()
+                    
+            } else {
+                bloquear = true; 
+                setTimeout (noPareja, 1000);        
+            }
+
+        }
+
+    }
+    
+    cartas.forEach (function (carta){
+        carta.addEventListener('click', flip )
+    })
+
+})    
+
+
